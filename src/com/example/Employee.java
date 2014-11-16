aba38906d889c1df4f68d2266bb775c46ba99e92
kailash gajulapalli <kailash@kailashs-MacBook-Pro.local>
2014-11-16 10:28:05 -0500

    first commit

---
+++ src/com/example/Employee.java
@@ -0,0 +1,33 @@
+package com.example;
+
+/**
+ * Created by kailash on 11/14/14.
+ */
+public class Employee {
+
+   private int id;
+    private String name;
+    private float salary;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+
+}
---
+++ src/com/example/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * Created by kailash on 11/11/14.
+ */
+public class HelloWorld {
+
+}
+
+
+
+
